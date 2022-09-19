292f3bee0ae1cf28187889de5e7a842b50e6c111
vladislavvakin1# <trevirnoah@gmail.com>
2022-09-19 23:37:38 +0100

    downloading a webpage as a string and saving in a local txt file

---
+++ є/є/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string text = "A class is the most powerfull data type in C#. Like a structure,  " +
+                            "a class defines the data and behavior of the data type. ";*/
+
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+            Console.WriteLine(reply);
+            System.IO.File.WriteAllText(@"C:\user-vladislav\all_folders\Writetext.txt", reply);
+        }
+    }
+}
